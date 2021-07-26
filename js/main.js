e56d66a408876f6777731d0528c2f77eced656ad
pflis <piotrek610@wp.pl>
2021-07-26 16:19:43 +0200

    zadanie33JS

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let paragraf = document.getElementById('opis');
+let przycisk = document.querySelector('button');
+document.querySelector('button').onclick = function() {
+    paragraf.textContent = 'dowolny tekst';
+}
